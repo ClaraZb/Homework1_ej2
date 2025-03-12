5449ceb0d9bf4fe2f56cee6b7188e44594631744
ClaraZb <czavaronibenoit@udesa.edu.ar>
2025-03-11 23:39:35 -0300

    correccion parte 2b

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -1,21 +1,61 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 using namespace std;
 
-void logMessage(string mensaje, string severidad){
+
+enum varios {CRITICAL = 1, ERROR, WARNING, INFO, DEBUG, SECURITY};
+
+string prioridad_a_string(int prioridad) {
+    switch (prioridad) {
+        case CRITICAL: return "CRITICAL";
+        case ERROR: return "ERROR";
+        case WARNING: return "WARNING";
+        case INFO: return "INFO";
+        case DEBUG: return "DEBUG";
+        case SECURITY: return "SECURITY";
+        default: return "UNKNOWN"; 
+    }
+} //vale la pena usar enum?
+
+void logMessage(string mensaje, int prioridad){
+
+    ofstream outFile("logreport", ios :: app);
+    if (outFile.is_open()){
+        outFile << "[" + prioridad_a_string(prioridad) + "] " + mensaje << endl; 
+        outFile.close();
+    }
+    else cerr << "Error al abrir el archivo"; 
+}
+
+//las siguientes funciones corresponden al ejercicio 2b
+void logMessage(string mensaje, string archivo, int linea){
     ofstream outFile("logreport", ios::app);
     if (outFile.is_open()){
-        outFile << "[" + severidad + "] " + mensaje << endl;
+        outFile << mensaje +" (" + archivo + " - l" + to_string(linea) + ")" << endl; //deberia contener la etiqueta [ERROR]?
+        outFile.close();
+    }
+    else cerr << "Error al abrir el archivo";
+}
+
+void logMessage(string mensaje, string usuario){
+    ofstream outFile("logreport", ios::app);
+    if (outFile.is_open()){
+        outFile << "[SECURITY] " + mensaje +" to " + usuario << endl; //deberia contener la etiqueta [ERROR]?
         outFile.close();
     }
     else cerr << "Error al abrir el archivo";
 }
 
 int main(){
-    logMessage("Printeamos variables", "DEBUG");
-    logMessage("Todo ok", "INFO");
-    logMessage("Tiempo de espera superior al esperado", "WARNING");
-    logMessage("Mal resultado", "ERROR");
-    logMessage("Multiples errores acumulados", "CRITICAL");
+    logMessage("Printeamos variables", varios::DEBUG);
+    logMessage("Todo ok", varios::INFO);
+    logMessage("Tiempo de espera superior al esperado", varios::WARNING);
+    logMessage("Mal resultado", varios::ERROR);
+    logMessage("Multiples errores acumulados", varios::CRITICAL);
+    
+    //comandos para la parte 2b
+    logMessage("Resultado no es correcto", "Potencias", 23);
+    logMessage("Access granted", "Harry Potter");
     return 0;
 }
